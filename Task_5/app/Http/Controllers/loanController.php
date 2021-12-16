084c0191eb8d30b216edc80c52d2e3b63da439cf
Mashiur88 <mashiur1803@gmail.com>
2021-12-16 20:59:18 +0600

    second commit

---
+++ Task_5/app/Http/Controllers/loanController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\File;
+use App\Models\Loan;
+
+
+class loanController extends Controller
+{
+    //
+    function rqstsubmit(Request $request)
+    {
+
+        $image = $request->file('document');
+        $image_name=$image->getClientOriginalName();
+        $image_ext=$image->getClientOriginalExtension();
+        $image_new_name =strtoupper(Str::random(6));
+        $image_full_name=$image_new_name.'.'.$image_ext;
+        $upload_path='storage/employee/';
+        $image_url=$upload_path.$image_full_name;
+        $success=$image->move($upload_path,$image_full_name);
+        $imageData='storage/employee/'.$image_full_name;
+
+        $loan=new Loan();
+        $loan->loantype=$request->loantype;
+        $loan->loanamount=$request->loan;
+        $loan->loaninterestrate=$request->irate;
+        $loan->amountpaid=$request->apaid;
+        $loan->loanapprovedate=$request->aprvdate;
+        $loan->loandocument=$imageData;
+        $loan->save();
+    }
+    function loanList()
+    {
+        return Loan::all();
+    }
+    function loandetails(Request $request)
+    {
+        $id=$request->id;
+        return Loan::where('id',$id)->limit(1)->get();
+
+    }
+}
